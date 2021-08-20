9c5e619997d125f3241e62c633b9d2f9b8ddf750
Mohammad Zafar <sz7861@outlook.com>
2021-08-20 23:24:30 +0100

    Create html routes, start server

--- server.js
+++ server.js
@@ -58,4 +58,16 @@ app.delete('/api/notes/:id', (req, res) => {
       }  
     })
 });
+// Add index.html file route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+}); 
+// Add notes.html file route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'));
+}); 
+// Start server
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
 
