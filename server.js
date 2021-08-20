ae18c45dbc86050fac74da6c4ff02505b9ec19a2
Mohammad Zafar <sz7861@outlook.com>
2021-08-20 23:21:02 +0100

    Function to write notes and add to db.json

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ app.use(express.json());
 app.use(express.static('public')); 
 // Require db.json to store notes
 const{notes} = require('./db/db.json');
+// Access notes input via req.body and add to db.json
+function writeNote (body, previousNotesList) {
+    const note = body; 
+    previousNotesList.push(note); 
+// Write file & return note to post route
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes : previousNotesList}, null, 2));
+    return note; 
+};
