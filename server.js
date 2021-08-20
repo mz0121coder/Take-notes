a091a055aa61e3d62aa86da65d622586e6af96e1
Mohammad Zafar <sz7861@outlook.com>
2021-08-20 23:18:41 +0100

    Parse data + require db.json to store notes

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001; 
 const app = express(); 
+// Parse data
+app.use(express.urlencoded ({extended: true}));
+app.use(express.json());
+app.use(express.static('public')); 
+// Require db.json to store notes
+const{notes} = require('./db/db.json');
